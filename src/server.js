ac3eef145e7064dd68708af0844f7d68dd9e8494
Max Gilbert <max@maxgilbert.co.uk>
2019-02-01 16:07:23 +0000

    WIP - Dummying up data
    Dummying up the data to allow basic queries.

--- src/server.js
+++ src/server.js
@@ -1,65 +1,59 @@
-import { ApolloServer } from 'apollo-server'
-import { loadTypeSchema } from './utils/schema'
-import { merge } from 'lodash'
+import { ApolloServer, gql } from 'apollo-server'
 import config from './config'
-import { connect } from './db'
-import product from './types/product/product.resolvers'
-import coupon from './types/coupon/coupon.resolvers'
-import user from './types/user/user.resolvers'
 
-const types = ['product', 'coupon', 'user']
+const types = ['user']
 
 export const start = async () => {
-  const rootSchema = `
+  const typeDefs = gql`
     type User {
-        id: Int!
-        name:Sring!
-
+        id: ID!
+        name:String!
+        permissions : [Permission]
     }
 
     type Permission {
-        id: Int!
-        name:Sring!
+      id: ID!
+      name: String!
     }
 
     type Query {
-        user: User
-        users: [User]!
-        permission: Permission
-        permissions: [Permission]!
-      }
+      user(id: Int, name: String) : User
+      users(name: String) : [User]
+    }
 
     schema {
       query: Query
     }
   `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
-
-  const userResolver = {
+  const resolvers = {
     Query: {
       user() {
         return { id: 1, name: 'Max Gilbert' }
       },
       users() {
         return [
-          { id: 1, name: 'Max Gilbert' },
-          { id: 2, name: 'Souban Quadri' },
-          { id: 3, name: 'Matthew Rothwell' }
+          { 
+            id: 1, name: 'Max Gilbert',
+            permissions: [
+              { 
+                id: 1,
+                name : "Can add things"
+              }
+            ]
+          },
+          { id: 2, name: 'Souban Quadri', permissions : []},
+          { id: 3, name: 'Matthew Rothwell', permissions : [] }
         ]
       }
     }
   }
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema],
-    resolvers: userResolver,
-    context({ req }) {
-      // use the authenticate function from utils to auth req, its Async!
-      return { user: null }
-    }
+    typeDefs,
+    resolvers,
   })
 
-  await connect(config.dbUrl)
+
   const { url } = await server.listen({ port: config.port })
 
   console.log(`GQL server ready at ${url}`)
